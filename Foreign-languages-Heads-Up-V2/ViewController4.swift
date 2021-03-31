0a2b6385437705c3c6a3b77fe7e20b3e3850fd0e
beltcroods <beltcroods@gmail.com>
2021-03-31 16:19:36 -0400

    First Commit

---
+++ Foreign-languages-Heads-Up-V2/ViewController4.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController4.swift
+//  Foreign-languages-Heads-Up-V2
+//
+//  Created by user180891 on 3/31/21.
+//  Copyright © 2021 user180891. All rights reserved.
+//
+
+import UIKit
+
+class ViewController4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
