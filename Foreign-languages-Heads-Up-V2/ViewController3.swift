f871641047ffe01b5b02628e5f2320caeedd9fb1
Kellen shuck <kshuck3398@stu.d214.org>
2021-04-05 15:54:49 -0400

    fixed CorrectArray

--- Foreign-languages-Heads-Up-V2/ViewController3.swift
+++ Foreign-languages-Heads-Up-V2/ViewController3.swift
@@ -18,7 +18,7 @@ class ViewController3: UIViewController {
     @IBOutlet weak var DisplayLabel: UILabel!
     
     var FrenchCardSet = ["une Bande Origianle", "Une Chaine", "Un Vidéoclip", "Un Divertissement", "Un Documentaire", "l'écran", "Les Effets Spéciaux", "Une Interview", "Un Feuilleton", "Une Premiére", "Les Sous-Titres", "Divertir", "Enregistrer", "Retransmettre", "Sortir Un Film"]
-     var CorrectArray = [""]
+    var CorrectArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
