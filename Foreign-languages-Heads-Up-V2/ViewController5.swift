c64a1db696d10de5c4bbb7ce2855fb56f34fc05c
Kellen shuck <kshuck3398@stu.d214.org>
2021-04-04 22:45:06 -0400

    Added

--- Foreign-languages-Heads-Up-V2/ViewController5.swift
+++ Foreign-languages-Heads-Up-V2/ViewController5.swift
@@ -8,23 +8,23 @@
 
 import UIKit
 
-class ViewController5: UIViewController {
+class ViewController5: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
 }
