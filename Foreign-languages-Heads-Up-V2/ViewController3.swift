716945c7d97abc53d0c1a13a7ae1dde5542037a6
Kellen shuck <kshuck3398@stu.d214.org>
2021-04-04 22:35:08 -0400

    fixed display on 3rd view controller, Fixed swipe gestures on 3rd view controller

--- Foreign-languages-Heads-Up-V2/ViewController3.swift
+++ Foreign-languages-Heads-Up-V2/ViewController3.swift
@@ -10,21 +10,31 @@ import UIKit
 
 class ViewController3: UIViewController {
 
+    @IBOutlet weak var NumberPassedLabel: UILabel!
+    @IBOutlet weak var PassedLabel: UILabel!
+    @IBOutlet weak var CorrectLabel: UILabel!
+    @IBOutlet weak var NumberCorrectLabel: UILabel!
+    @IBOutlet weak var TimerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func SwipeRight(_ sender: Any) {
+        var PassedNumber = Int(NumberPassedLabel.text!)!
+               PassedNumber += 1
+               var Pnumber = String(PassedNumber)
+               NumberPassedLabel.text = Pnumber
     }
-    */
-
+    
+   
+    @IBAction func SwipeLeft(_ sender: Any) {
+        var CorrectNumber = Int(NumberCorrectLabel.text!)!
+        CorrectNumber += 1
+        var cnumber = String(CorrectNumber)
+        NumberCorrectLabel.text = cnumber
+    }
+    
 }
