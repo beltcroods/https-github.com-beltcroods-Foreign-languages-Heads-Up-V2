baa2313cafb5acf6988467cdcd1659fa47e04822
beltcroods <beltcroods@gmail.com>
2021-04-21 16:15:55 -0400

    Randomizer Completed

--- Foreign-languages-Heads-Up-V2/ViewController3.swift
+++ Foreign-languages-Heads-Up-V2/ViewController3.swift
@@ -17,8 +17,23 @@ class ViewController3: UIViewController {
     @IBOutlet weak var TimerLabel: UILabel!
     @IBOutlet weak var DisplayLabel: UILabel!
     
-    var FrenchCardSet = ["une Bande Origianle", "Une Chaine", "Un Vidéoclip", "Un Divertissement", "Un Documentaire", "l'écran", "Les Effets Spéciaux", "Une Interview", "Un Feuilleton", "Une Premiére", "Les Sous-Titres", "Divertir", "Enregistrer", "Retransmettre", "Sortir Un Film"]
+    let cardset1 = ["soundtrack": "une bande originale",
+                    "network" : "une chaine",
+                    "music video" : "un clip video ; un videoclip",
+                    "entertainment" : "un divertissment",
+                    "documentary" : "un documentaire",
+                            "screen" : "pecran",
+                            "special effects" : "les effets speciaux (m)",
+                            "interview" : "un entretien/une unterview",
+                            "soap opera ; series" : "un feuilleton",
+                            "premiere" : "une premoere",
+                            "subtitles" : "les sous-titres (m)",
+                            "to entertain" : "divertir",
+                            "to broadcast" : "retransmettre",
+                            "to release a movie" : "sortir un film"]
     var CorrectArray: [String] = []
+    
+    
     
     var counter = 100
     
@@ -57,9 +72,11 @@ class ViewController3: UIViewController {
     }
 })
         
+        DisplayLabel.text = cardset1["soundtrack"]
         
 }
 
+    
     
     
     @IBAction func SwipeRight(_ sender: Any) {
@@ -67,19 +84,99 @@ class ViewController3: UIViewController {
                PassedNumber += 1
                var Pnumber = String(PassedNumber)
                NumberPassedLabel.text = Pnumber
+            
         
-        
+                let CurrentCardNumber = Int.random(in: 1...13)
+                if CurrentCardNumber == 1 {
+                    DisplayLabel.text = cardset1["soundtrack"]
+                }
+                if CurrentCardNumber == 2 {
+                    DisplayLabel.text = cardset1["network"]
+                }
+                if CurrentCardNumber == 3 {
+                    DisplayLabel.text = cardset1["music video"]
+                }
+                if CurrentCardNumber == 4 {
+                   DisplayLabel.text = cardset1["documentary"]
+                }
+                if CurrentCardNumber == 5 {
+                   DisplayLabel.text = cardset1["screen"]
+                }
+                if CurrentCardNumber == 6 {
+                    DisplayLabel.text = cardset1["special effects"]
+                }
+                if CurrentCardNumber == 7 {
+                    DisplayLabel.text = cardset1["interview"]
+                }
+                if CurrentCardNumber == 8 {
+                    DisplayLabel.text = cardset1["soap opera"]
+                }
+                if CurrentCardNumber == 9 {
+                    DisplayLabel.text = cardset1["premiere"]
+                }
+                if CurrentCardNumber == 10 {
+                    DisplayLabel.text = cardset1["subtitles"]
+                }
+                if CurrentCardNumber == 11 {
+                    DisplayLabel.text = cardset1["to entertain"]
+                }
+                if CurrentCardNumber == 12 {
+                    DisplayLabel.text = cardset1["to broadcast"]
+                }
+                if CurrentCardNumber == 13 {
+                    DisplayLabel.text = cardset1["to release a movie"]
+                }
     }
-    
-   
     @IBAction func SwipeLeft(_ sender: Any) {
         var CorrectNumber = Int(NumberCorrectLabel.text!)!
         CorrectNumber += 1
         var cnumber = String(CorrectNumber)
         NumberCorrectLabel.text = cnumber
         
-        CorrectArray.append(contentsOf: [DisplayLabel.text!])
+        CorrectArray.append(contentsOf: [(DisplayLabel.text ?? "ERORR")])
         print(CorrectArray)
+        
+        let CurrentCardNumber = Int.random(in: 1...13)
+        if CurrentCardNumber == 1 {
+            DisplayLabel.text = cardset1["soundtrack"]
+        }
+        if CurrentCardNumber == 2 {
+            DisplayLabel.text = cardset1["network"]
+        }
+        if CurrentCardNumber == 3 {
+            DisplayLabel.text = cardset1["music video"]
+        }
+        if CurrentCardNumber == 4 {
+           DisplayLabel.text = cardset1["documentary"]
+        }
+        if CurrentCardNumber == 5 {
+           DisplayLabel.text = cardset1["screen"]
+        }
+        if CurrentCardNumber == 6 {
+            DisplayLabel.text = cardset1["special effects"]
+        }
+        if CurrentCardNumber == 7 {
+            DisplayLabel.text = cardset1["interview"]
+        }
+        if CurrentCardNumber == 8 {
+            DisplayLabel.text = cardset1["soap opera"]
+        }
+        if CurrentCardNumber == 9 {
+            DisplayLabel.text = cardset1["premiere"]
+        }
+        if CurrentCardNumber == 10 {
+            DisplayLabel.text = cardset1["subtitles"]
+        }
+        if CurrentCardNumber == 11 {
+            DisplayLabel.text = cardset1["to entertain"]
+        }
+        if CurrentCardNumber == 12 {
+            DisplayLabel.text = cardset1["to broadcast"]
+        }
+        if CurrentCardNumber == 13 {
+            DisplayLabel.text = cardset1["to release a movie"]
+        }
+       
     }
     
     
